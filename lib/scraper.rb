83f606810a11492f63315a6c8abaedf0fafcada0
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-10-01 21:22:06 -0500

    scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
+
+doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/new-releases/_/N-1oyg"))
+doc.css("div.sidebar__section refinements")
+binding.pry
+
 end
+
+#genres - .css("div.refinements").text
+#url - .css("a.bread-crumbs__item").attribute("href").value
